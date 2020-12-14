5e875d21ec888edec83c656198716e292420a782
Gerard <gerar.f87@gmail.com>
2020-12-14 20:05:58 +0100

    Allow change clock font

--- app/src/main/java/com/example/clockwidget/widgetprovider/LayoutController.kt
+++ app/src/main/java/com/example/clockwidget/widgetprovider/LayoutController.kt
@@ -4,7 +4,25 @@ import android.content.SharedPreferences
 import com.example.clockwidget.R
 
 const val SHADOW_KEY: String = "shadow"
+const val FONT_KEY: String = "font"
 
-fun getLayout(sharedPreferences: SharedPreferences): Int =
-        if (sharedPreferences.getBoolean(SHADOW_KEY, true))
-            R.layout.appwidget_provider_layout_shadows else R.layout.appwidget_provider_layout
+fun getLayout(sharedPreferences: SharedPreferences): Int {
+    val shadow: Boolean = sharedPreferences.getBoolean(SHADOW_KEY, true)
+    val font: String = sharedPreferences.getString(FONT_KEY, "0").toString()
+
+    return if (shadow) {
+        when (font) {
+            "0" -> R.layout.appwidget_provider_layout_google_sans_shadows
+            "1" -> R.layout.appwidget_provider_layout_open_sans_shadows
+            "2" -> R.layout.appwidget_provider_layout_montserrat_shadows
+            else -> R.layout.appwidget_provider_layout_google_sans_shadows
+        }
+    } else {
+        when (font) {
+            "0" -> R.layout.appwidget_provider_layout_google_sans
+            "1" -> R.layout.appwidget_provider_layout_open_sans
+            "2" -> R.layout.appwidget_provider_layout_montserrat
+            else -> R.layout.appwidget_provider_layout_google_sans
+        }
+    }
+}
