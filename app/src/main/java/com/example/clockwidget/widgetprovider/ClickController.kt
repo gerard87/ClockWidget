76ff85006fca08c6807e6f1e903c1c23be1177ac
Gerard <gerar.f87@gmail.com>
2021-04-02 18:37:52 +0200

    Search for deskclock package

--- app/src/main/java/com/example/clockwidget/widgetprovider/ClickController.kt
+++ app/src/main/java/com/example/clockwidget/widgetprovider/ClickController.kt
@@ -3,11 +3,18 @@ package com.example.clockwidget.widgetprovider
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.content.pm.ApplicationInfo
+import android.content.pm.PackageManager
 
-const val CLICK_PACKAGE: String = "com.google.android.deskclock"
-
-private fun getOpenIntent(context: Context): Intent? =
-        context.packageManager.getLaunchIntentForPackage(CLICK_PACKAGE)
+private fun getOpenIntent(context: Context): Intent? {
+    val apps: MutableList<ApplicationInfo> =
+        context.packageManager?.getInstalledApplications(PackageManager.GET_META_DATA) as MutableList<ApplicationInfo>
+    var pck: String = "com.google.android.deskclock"
+    for (app in apps) {
+        if (app.packageName.contains("deskclock")) pck = app.packageName
+    }
+    return context.packageManager.getLaunchIntentForPackage(pck)
+}
 
 fun getPendingIntent(context: Context) : PendingIntent {
     // Create an Intent to launch OpenActivity
